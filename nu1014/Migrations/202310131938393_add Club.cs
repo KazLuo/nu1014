d36ffd0f3731e427c984a5bf17b81c18b687c703
ricks774 <ricks774@gmail.com>
2023-10-14 03:39:28 +0800

    Add Club

---
+++ nu1014/Migrations/202310131938393_add Club.cs
@@ -0,0 +1,26 @@
+namespace nu1014.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addClub : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clubs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 100),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Clubs");
+        }
+    }
+}
